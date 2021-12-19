699a169d17b0f6bbf386ff534ad0c9791f225772
Your Name <you@example.com>
2021-12-19 22:17:45 +0530

    new commit

---
+++ javaPractice/src/Methods.java
@@ -0,0 +1,12 @@
+
+public class Methods {
+
+	public  void ValidateHeader()
+	{
+		System.out.println("validated header links");
+
+		
+	
+	}
+
+}
---
+++ javaPractice/src/Parent.java
@@ -0,0 +1,25 @@
+
+public class Parent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			
+		//System.out.println("i started learning java");
+// add two numbers
+		
+	/*	int a,b,c;
+		a=2;b=3;
+		c=a+b;
+		System.out.println("sum is "+c);*/
+		
+		//3 lines of code header
+		System.out.println("i navigated to Home Page ");
+
+		Methods m=new Methods();
+		m.ValidateHeader();
+		
+				
+	}
+
+}
